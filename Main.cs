440edafcb541486612879f60803b0cab01397900
Biscuit <84092899+BiscuiTheHobkin@users.noreply.github.com>
2024-04-10 18:58:42 +0200

    Code upload

---
+++ Main.cs
@@ -0,0 +1,50 @@
+﻿using MelonLoader;
+using UnityEngine;
+using System;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+namespace Flip
+{
+    public static class BuildInfo
+    {
+        public const string Name = "FlipScreen";
+        public const string Description = "Ported version of the FScreen made by BlazeBest.";
+        public const string Author = "BlazeBest | Ported by Biscuit";
+        public const string Company = null;
+        public const string Version = "1.0";
+        public const string DownloadLink = null;
+    }
+
+    public class FlipScreen : MelonMod
+    {
+        public override void OnInitializeMelon() 
+        {
+         MelonLogger.Msg("FlipScreen Loaded !");
+         MelonLogger.Msg("Use your left control key + Mouse ScrollWheel");
+        }
+
+        public void Update()
+        {
+            if (!Input.GetKey(KeyCode.LeftControl)) return;
+
+            Camera camera = Camera.main;
+            if (camera == null) return;
+
+            float mouseWheel = Input.GetAxis("Mouse ScrollWheel");
+            if (mouseWheel > 0.1)
+            {
+                Quaternion quaternion = camera.transform.rotation;
+                Vector3 euler = quaternion.eulerAngles;
+                euler -= Vector3.forward;
+                camera.transform.rotation = Quaternion.Euler(euler);
+            }
+            else if (mouseWheel < -0.1)
+            {
+                Quaternion quaternion = camera.transform.rotation;
+                Vector3 euler = quaternion.eulerAngles;
+                euler += Vector3.forward;
+                camera.transform.rotation = Quaternion.Euler(euler);
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using MelonLoader;
+
+[assembly: AssemblyTitle(Flip.BuildInfo.Description)]
+[assembly: AssemblyDescription(Flip.BuildInfo.Description)]
+[assembly: AssemblyCompany(Flip.BuildInfo.Company)]
+[assembly: AssemblyProduct(Flip.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + Flip.BuildInfo.Author)]
+[assembly: AssemblyTrademark(Flip.BuildInfo.Company)]
+[assembly: AssemblyVersion(Flip.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(Flip.BuildInfo.Version)]
+[assembly: MelonInfo(typeof(Flip.FlipScreen), Flip.BuildInfo.Name, Flip.BuildInfo.Version, Flip.BuildInfo.Author, Flip.BuildInfo.DownloadLink)]
+[assembly: MelonColor()]
+
+// Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
+// If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
+// Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null, null)]
